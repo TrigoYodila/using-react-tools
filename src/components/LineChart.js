d0a0c013911f768067dc3d4fd892666d14e537f7
Trigo <trigoyodila1996@gmail.com>
2024-01-10 14:23:34 +0100

    fix customize axes

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -9,15 +9,61 @@ function LineChart() {
     datasets:[
         {
             label:'Sales for 2021 (M)',
-            data : [3,2,2,1,5]
+            data : [3,2,2,1,5],
+            borderColor:['rgba(255,206,86,0.2)'],
+            backgroundColor:['rgba(255,206,86,0.2)'],
+            pointBackgroundColor:'rgba(255,206,86,0.2)',
+            pointBorderColor:'rgba(255,206,86,0.2)'
+        },
+        {
+          label:'Sales for 2019 (M)',
+          data:[1,2,3,2,4]
         }
     ]
+  }
+
+  const options = {
+    animation:true, //default (false)
+    plugins:{
+      title:{
+        display:true,
+        text:'Line Chart'
+      },
+      tooltip:{
+        enabled:false
+      },
+      legend:{
+        display:true, // default (false)
+        position:"bottom"
+      },
+
+    },
+    scales:{ 
+      y:{
+        max:6,
+        min:0,
+        ticks:{
+          stepSize:1
+        }
+      }
+  },
+    // scales:{
+    //   y:[
+    //     {
+    //       ticks:{
+    //         min:0,
+    //         max:6,
+    //         stepSize:1
+    //       }
+    //     }
+    //   ]
+    // }
   }
 
   return (
     // <Line data={data}/>
     <div>
-      <Line data={data} />
+      <Line data={data} options={options}/>
     </div>
   )
 }
